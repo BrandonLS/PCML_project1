00b6ae2b5eb3c293b2947238db6ce21d70f6cfb5
Fearnley Martin <fearnleymartin@gmail.com>
2016-10-26 00:38:40 +0200

    fixing bugs in run.py

--- run.py
+++ run.py
@@ -1,47 +1,39 @@
 """
 ML pipeline
 """
-from scripts import logistic_regression
+from scripts import logistic_regression, sigmoid
+import numpy as np
+
+# TRAINING
 
 # Import data
-from proj1_helpers import *
-DATA_TRAIN_PATH = '/home/acy/PCML/projects/project1/train.csv' # TODO: download train data and supply path here 
+from helpers import *
+DATA_TRAIN_PATH = 'data/train.csv' # TODO: download train data and supply path here 
 y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
-# Clean data
-N = tX.shape[0]
-D = tX.shape[1]
-no_samples = N
-
-cnt = 0
-exclude = []
-for n in range(no_samples):
-    for d in range(D):
-        if tX[n,d] == -999.0:
-            exclude.append(n)
-            cnt = cnt + 1
-            break
-
-y_c=np.delete(y,exclude,0);
-tX_c=np.delete(tX,exclude,0); 
-
-# Standardise data
-tX_norm, mean, std = standardize(tX_c, None, None)
-y_shifted = np.array([1 if i==1 else 0 for i in y_c])
+# Clean and standardise data
+# We standardise the data matrix, add a column of 1s to the data matrix, and change the -1s to 0s in y
+# We then replace all the outliers (-999) with 0
+y_shifted, tX_norm = pre_process_data(y, tX)
 
 # Run logisitic regression algorithm
 max_iters = 100
 batch_size = N/100
 gamma=0.00035938136638046257
 
-loss, w = logistic_regression(y_c, tX_c, gamma, max_iters, batch_size)
+loss, w = logistic_regression(y_shifted, tX_norm, gamma, max_iters, batch_size)
 
-# Output predictions
+# PREDICTIONS
 
-DATA_TEST_PATH = '' # TODO: download train data and supply path here 
+#Load test data
+DATA_TEST_PATH = 'data/test.csv' # TODO: download train data and supply path here 
+OUTPUT_PATH = 'data/output2.csv' # TODO: fill in desired name of output file for submission
 _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
 
-OUTPUT_PATH = '' # TODO: fill in desired name of output file for submission
-y_pred = predict_labels(weights, tX_test)
-create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
+# Process test data
+_, tX_test = pre_process_data(_,tX_test)
 
+# Make predictions
+y_est = sigmoid(np.dot(tX_test,w))
+y_label = [-1 if i<0.5 else 1 for i in y_est]
+create_csv_submission(ids_test, y_label, OUTPUT_PATH)
